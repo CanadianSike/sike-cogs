f5d7a44bf4140a8c0ddce85841df49ea7d252e87
CanadianSike <lochlanc8@gmail.com>
2024-04-05 15:00:45 -0300

    Add files via upload

---
+++ pincog/__init__.py
@@ -0,0 +1,8 @@
+from .messagepinner import pincog
+import asyncio
+
+
+async def setup(bot):
+    obj = bot.add_cog(pincog())
+    if asyncio.iscoroutine(obj):
+        await obj
---
+++ pincog/messagepinner.py
@@ -0,0 +1,20 @@
+from redbot.core import commands, checks, Config, app_commands
+import discord
+
+class PinCog(commands.Cog):
+    """Custom Pinning Cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def sike(self, ctx):
+        await ctx.send("SikeCogs Has been loaded. Good luck!")
+
+
+    @checks.mod_or_permissions(manage_messages=True)
+    @commands.guild_only()
+    @commands.command()
+    async def pin_message(ctx, msg_id):
+        msg_id = await ctx.fetch_message(msg_id)
+        await ctx.message.pin(msg_id)
