0e07958a832b538725bf7402488bd1dbdaed8c4e
CanadianSike <lochlanc8@gmail.com>
2024-04-08 14:08:31 -0300

    hmm

--- pincog/pincog.py
+++ pincog/pincog.py
@@ -3,8 +3,19 @@ from redbot.core.i18n import Translator, cog_i18n
 import discord
 
 
+
+
+@cog_i18n(_)
 class PinCog(commands.Cog):
     """Custom Pinning Cog"""
+
+    default_channel = {}
+    default_guild = {}
+
+    def __init__(self):
+        self.settings = Config.get_conf(self, identifier=59595922, force_registration=True)
+        self.settings.register_channel(**self.default_channel)
+        self.settings.register_guild(**self.default_guild)
 
     @commands.command()
     async def sike(self, ctx):
@@ -15,6 +26,6 @@ class PinCog(commands.Cog):
     @commands.guild_only()
     @commands.command()
     async def pinmsg(ctx, msg_id: int):
-        message = await ctx.get_partial_messageable(msg_id)
+        message = await ctx.fetch_message(msg_id)
         await message.pin(msg_id)
         
