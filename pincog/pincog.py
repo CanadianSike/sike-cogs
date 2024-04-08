a1ba951c960219460dfc169f02b0fd14c4d16d9e
CanadianSike <lochlanc8@gmail.com>
2024-04-08 11:39:47 -0300

    sigh

--- pincog/pincog.py
+++ pincog/pincog.py
@@ -12,6 +12,6 @@ class PinCog(commands.Cog):
     @checks.mod_or_permissions(manage_messages=True)
     @commands.guild_only()
     @commands.command()
-    async def pinmsg(ctx, msg_id: int):
+    async def pinmsg(self, ctx, msg_id: int):
         msg = await ctx.fetch_message(msg_id)
         await ctx.message.pin(msg)
