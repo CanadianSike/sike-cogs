36d5ef8409ffa728398a275d8900ab5353d52ffa
CanadianSike <lochlanc8@gmail.com>
2024-04-09 12:17:05 -0300

    innit fixes?

--- pincog/__init__.py
+++ pincog/__init__.py
@@ -1,10 +1,6 @@
-
-from redbot.core.bot import Red
 from .pincog import PinCog
 import asyncio
 
 
 async def setup(bot):
-    cog = PinCog(bot)
-    await bot.add_cog(cog)
-     # await cog.initialize()
+    await bot.add_cog(PinCog())
