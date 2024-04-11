416a1d16878674ae4a9cbd2ac971a2f5793fc6b4
CanadianSike <lochlanc8@gmail.com>
2024-04-11 10:44:58 -0300

    trying fixes

--- pincog/pincog.py
+++ pincog/pincog.py
@@ -18,10 +18,10 @@ class PinCog(commands.Cog):
         await ctx.send("SikeCogs Has been loaded. Good luck!")
 
     @commands.command()
-    async def roleset(self, ctx, id):
+    async def roleset(self, ctx, id: int, roleid: int):
         """Please submit role IDs for pinmsg permissions."""
-        id = get(ctx.guild.roles, roleid = None)
-        await ctx.send(content=id)
+        roleid = get(ctx.guild.roles, id = int)
+        await ctx.send(content=roleid)
 
         #@discord.app_commands.checks.has_role(role_id: int)
         #def check_roles(role_id):
